2ed6b77fda1e20cd2935670716b7d961a2c1fc3a
HokieGeek <andres.f.perez@gmail.com>
2017-08-11 07:02:46 -0400

    Looking at how to flesh this out

--- src/app/lab.service.ts
+++ src/app/lab.service.ts
@@ -2,10 +2,18 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Lab {
+    server: string = "http://localhost:8081";
+    // experiments: { [id: string] : void; } = {}
 
     constructor() { }
 
     newExperiment() {
         console.log("TODO: new experiment")
+        // TODO: deal with callback
+        // $.post(server+"/analyze", JSON.stringify(req))
+        //     .done(callback)
+        //     .fail(function(err) {
+        //         console.log("Got a post error:", err.status, err.responseText);
+        //     });
     }
 }
--- src/app/petridish/petridish.component.ts
+++ src/app/petridish/petridish.component.ts
@@ -18,4 +18,40 @@ export class PetridishComponent implements OnInit {
     ngOnInit() {
     }
 
+    // function generateRandomSeed(id, boardSize, cellSize, coverage) {
+    //     // console.log("generateRandomSeed()", id, boardSize, cellSize, coverage);
+    //     var seed = [];
+
+    //     var adjWidth = cellSize.width + cellSpacing;
+    //     var adjHeight = cellSize.height + cellSpacing;
+
+    //     var row;
+    //     var col;
+
+    //     var board = document.getElementById(id);
+    //     var ctx = board.getContext('2d');
+    //     ctx.save();
+
+    //     ctx.setTransform(1, 0, 0, 1, 0, 0);
+    //     ctx.clearRect(0, 0, board.width, board.height);
+
+    //     ctx.fillStyle = cellAliveColor;
+
+    //     var aliveVal = 100-coverage;
+    //     for (row = boardSize.Height-1; row >= 0; row--) {
+    //         for (col = boardSize.Width-1; col >= 0; col--) {
+    //             if ((Math.random() * 100) > aliveVal) {
+
+    //                 ctx.fillRect(col * adjWidth,
+    //                     row * adjHeight,
+    //                     cellSize.width, cellSize.height);
+
+    //                 seed.push({"X": col, "Y": row});
+    //             }
+    //         }
+    //     }
+    //     ctx.restore();
+
+    //     return seed;
+    // }
 }
