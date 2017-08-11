7fa1614175e6273d500b902866d08a74945a79cf
HokieGeek <andres.f.perez@gmail.com>
2017-08-11 15:40:04 -0400

    Got the canvas drawing

--- src/app/dishrack/dishrack.component.ts
+++ src/app/dishrack/dishrack.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Cell } from '../cell'
+
 @Component({
   selector: 'dishrack',
   templateUrl: './dishrack.component.html',
   styleUrls: ['./dishrack.component.css']
 })
 export class DishrackComponent implements OnInit {
+    width: number = 50
+    height: number = 50
+    dummy: Cell[]
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.dummy = []
+        for (var row = this.height-1; row >= 0; row--) {
+            for (var col = this.width-1; col >= 0; col--) {
+                // if (row % 2 == 0 && col % 2 != 0) {
+                // if (row % 2 == 0) {
+                    this.dummy.push(new Cell(col, row));
+                // }
+            }
+        }
+    //     var aliveVal = 100-coverage;
+    //     for (row = boardSize.Height-1; row >= 0; row--) {
+    //         for (col = boardSize.Width-1; col >= 0; col--) {
+    //             if ((Math.random() * 100) > aliveVal) {
 
+    //                 ctx.fillRect(col * adjWidth,
+    //                     row * adjHeight,
+    //                     cellSize.width, cellSize.height);
+
+    //                 seed.push({"X": col, "Y": row});
+    //             }
+    //         }
+    //     }
+    }
 }
--- src/app/petridish/petridish.component.ts
+++ src/app/petridish/petridish.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 
 import { Lab } from '../lab.service';
+import { Cell } from '../cell'
 
 @Component({
   selector: 'petridish',
@@ -8,50 +9,91 @@ import { Lab } from '../lab.service';
   styleUrls: ['./petridish.component.css']
 })
 export class PetridishComponent implements OnInit {
+    @ViewChild('board') private board
+    lifeCells: Cell[]
+    boardWidth: number
+    boardHeight: number
 
-    lab: Lab
+    cellAliveColor: string = '#4863a0'; // FIXME: hmmm
+    cellDeadColor: string = '#e0e0e0';
+    cellSpacing: number = 1;
+    cellSize: number = 2
 
-    constructor(lab: Lab) {
-        this.lab = lab
+    cellSpace() {
+        return this.cellSize + this.cellSpacing
+    }
+
+    @Input()
+    set width(w) {
+        this.boardWidth = (w * this.cellSpace()) - this.cellSpacing
+        console.log("width", w, this.boardWidth)
+    }
+
+    get width() {
+        return this.boardWidth
+    }
+
+    @Input()
+    set height(h) {
+        this.boardHeight = (h * this.cellSpace()) - this.cellSpacing
+        console.log("height", h, this.boardHeight)
+    }
+
+    get height() {
+        return this.boardHeight
+    }
+
+    @Input()
+    set cells(cells) {
+        this.lifeCells = cells
+        this.draw()
+    }
+
+    get cells() {
+        return this.lifeCells
+    }
+
+    constructor() {
     }
 
     ngOnInit() {
+        // this.board.nativeElement.resizable({
+        //   helper: "resizable-helper",
+        //   // resize: function(event, ui) { console.log("resizing w,h: ", ui.size.width, ui.size.height); },
+        //   stop: function(event, ui) { this.boardWidth = ui.size.width; this.boardHeight = ui.size.height; draw(); }
+        // });
     }
 
-    // function generateRandomSeed(id, boardSize, cellSize, coverage) {
-    //     // console.log("generateRandomSeed()", id, boardSize, cellSize, coverage);
-    //     var seed = [];
+    private draw() {
+        console.log("draw()")
 
-    //     var adjWidth = cellSize.width + cellSpacing;
-    //     var adjHeight = cellSize.height + cellSpacing;
+        var ctx = this.board.nativeElement.getContext('2d')
+        ctx.save()
 
-    //     var row;
-    //     var col;
+        ctx.setTransform(1, 0, 0, 1, 0, 0)
+        ctx.clearRect(0, 0, this.width, this.height)
+        // ctx.clearRect(0, 0, this.board.nativeElement.width, this.board.nativeElement.height)
+        console.log("size", this.width, this.height)
+        console.log("size2", this.board.nativeElement.width, this.board.nativeElement.height)
+        console.log("num cells", this.cells.length)
 
-    //     var board = document.getElementById(id);
-    //     var ctx = board.getContext('2d');
-    //     ctx.save();
+        var cnt = 0
+        ctx.fillStyle = this.cellAliveColor
+        for (var alive in this.cells) {
+            cnt++
+            // console.log(this.cells[alive], this.cells[alive].X * this.cellSpace(),
+            //                                 this.cells[alive].Y * this.cellSpace())
+            ctx.fillRect(this.cells[alive].X * this.cellSpace(),
+                         this.cells[alive].Y * this.cellSpace(),
+                         this.cellSize,
+                         this.cellSize)
+        }
 
-    //     ctx.setTransform(1, 0, 0, 1, 0, 0);
-    //     ctx.clearRect(0, 0, board.width, board.height);
+        ctx.fillRect(100, 75, 10, 10)
 
-    //     ctx.fillStyle = cellAliveColor;
+        console.log("num rects", cnt)
+        console.log(0, this.cells[0], this.cells[0].X * this.cellSpace(), this.cells[0].Y * this.cellSpace())
 
-    //     var aliveVal = 100-coverage;
-    //     for (row = boardSize.Height-1; row >= 0; row--) {
-    //         for (col = boardSize.Width-1; col >= 0; col--) {
-    //             if ((Math.random() * 100) > aliveVal) {
-
-    //                 ctx.fillRect(col * adjWidth,
-    //                     row * adjHeight,
-    //                     cellSize.width, cellSize.height);
-
-    //                 seed.push({"X": col, "Y": row});
-    //             }
-    //         }
-    //     }
-    //     ctx.restore();
-
-    //     return seed;
-    // }
+        ctx.restore()
+    }
 }
