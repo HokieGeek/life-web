b8a9888fcbe09669ae65e306a8e5c5b995222234
HokieGeek <andres.f.perez@gmail.com>
2017-08-11 19:28:52 -0400

    Got the canvas size fixed!

--- src/app/petridish/petridish.component.ts
+++ src/app/petridish/petridish.component.ts
@@ -11,13 +11,11 @@ import { Cell } from '../cell'
 export class PetridishComponent implements OnInit {
     @ViewChild('board') private board
     lifeCells: Cell[]
-    boardWidth: number
-    boardHeight: number
 
     cellAliveColor: string = '#4863a0'; // FIXME: hmmm
     cellDeadColor: string = '#e0e0e0';
     cellSpacing: number = 1;
-    cellSize: number = 2
+    cellSize: number = 3
 
     cellSpace() {
         return this.cellSize + this.cellSpacing
@@ -25,22 +23,22 @@ export class PetridishComponent implements OnInit {
 
     @Input()
     set width(w) {
-        this.boardWidth = (w * this.cellSpace()) - this.cellSpacing
-        console.log("width", w, this.boardWidth)
+        this.board.nativeElement.width = (w * this.cellSpace()) - this.cellSpacing
+        // console.log("width", w, this.board.nativeElement.width)
     }
 
     get width() {
-        return this.boardWidth
+        return this.board.nativeElement.width
     }
 
     @Input()
     set height(h) {
-        this.boardHeight = (h * this.cellSpace()) - this.cellSpacing
-        console.log("height", h, this.boardHeight)
+        this.board.nativeElement.height = (h * this.cellSpace()) - this.cellSpacing
+        // console.log("height", h, this.board.nativeElement.height)
     }
 
     get height() {
-        return this.boardHeight
+        return this.board.nativeElement.height
     }
 
     @Input()
@@ -62,37 +60,25 @@ export class PetridishComponent implements OnInit {
         //   // resize: function(event, ui) { console.log("resizing w,h: ", ui.size.width, ui.size.height); },
         //   stop: function(event, ui) { this.boardWidth = ui.size.width; this.boardHeight = ui.size.height; draw(); }
         // });
+        // console.log("size", this.board.nativeElement.width, this.board.nativeElement.height)
+        // this.board.nativeElement.width = 150
+        // console.log("size", this.board.nativeElement.width, this.board.nativeElement.height)
     }
 
     private draw() {
-        console.log("draw()")
-
         var ctx = this.board.nativeElement.getContext('2d')
         ctx.save()
 
         ctx.setTransform(1, 0, 0, 1, 0, 0)
         ctx.clearRect(0, 0, this.width, this.height)
-        // ctx.clearRect(0, 0, this.board.nativeElement.width, this.board.nativeElement.height)
-        console.log("size", this.width, this.height)
-        console.log("size2", this.board.nativeElement.width, this.board.nativeElement.height)
-        console.log("num cells", this.cells.length)
 
-        var cnt = 0
         ctx.fillStyle = this.cellAliveColor
         for (var alive in this.cells) {
-            cnt++
-            // console.log(this.cells[alive], this.cells[alive].X * this.cellSpace(),
-            //                                 this.cells[alive].Y * this.cellSpace())
             ctx.fillRect(this.cells[alive].X * this.cellSpace(),
                          this.cells[alive].Y * this.cellSpace(),
                          this.cellSize,
                          this.cellSize)
         }
-
-        ctx.fillRect(100, 75, 10, 10)
-
-        console.log("num rects", cnt)
-        console.log(0, this.cells[0], this.cells[0].X * this.cellSpace(), this.cells[0].Y * this.cellSpace())
 
         ctx.restore()
     }
