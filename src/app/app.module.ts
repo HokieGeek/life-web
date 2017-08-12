b07bb136cc93e1718ceab870a3f798c7f80dbc90
HokieGeek <andres.f.perez@gmail.com>
2017-08-12 11:45:50 -0400

    Much more usable board and stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ControlsComponent } from './controls/controls.component';
 import { PetridishComponent } from './petridish/petridish.component';
+import { BoardComponent } from './petridish/board/board.component';
 import { DishrackComponent } from './dishrack/dishrack.component';
 
 import { Lab } from './lab.service';
@@ -13,10 +16,13 @@ import { Lab } from './lab.service';
     AppComponent,
     ControlsComponent,
     PetridishComponent,
+    BoardComponent,
     DishrackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
   ],
   providers: [Lab],
   bootstrap: [AppComponent]
